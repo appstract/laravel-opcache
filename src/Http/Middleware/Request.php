c25d48cf5ef86be4b2ed0b661b63f13f1f2fc19c
Olav van Schie <olavschie@hotmail.com>
2017-05-26 16:24:24 +0200

    AuthorizationException does not exist in 5.1

--- src/Http/Middleware/Request.php
+++ src/Http/Middleware/Request.php
@@ -3,7 +3,7 @@
 namespace Appstract\Opcache\Http\Middleware;
 
 use Closure;
-use Illuminate\Auth\Access\AuthorizationException;
+use Exception;
 
 /**
  * Class Request.
@@ -17,12 +17,12 @@ class Request
      * @param Closure $next
      *
      * @return mixed
-     * @throws AuthorizationException
+     * @throws Exception
      */
     public function handle($request, Closure $next)
     {
         if (! in_array($this->getRequestIp($request), [$_SERVER['SERVER_ADDR'], '127.0.0.1', '::1'])) {
-            throw new AuthorizationException('This action is unauthorized.');
+            throw new Exception('This action is unauthorized.');
         }
 
         return $next($request);
