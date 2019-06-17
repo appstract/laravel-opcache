2f3f710dd1a2f79526cd30b30af7ac00427e5066
ovanschie <olavschie@hotmail.com>
2019-06-17 22:12:59 +0200

    remove lumen routes

--- src/OpcacheServiceProvider.php
+++ src/OpcacheServiceProvider.php
@@ -35,33 +35,13 @@ class OpcacheServiceProvider extends ServiceProvider
         // config
         $this->mergeConfigFrom(__DIR__.'/../config/opcache.php', 'opcache');
 
-        if ($this->isLumenWithoutRouterVersion()) {
-            $router = $this->app;
-        } else {
-            $router = $this->app->router;
-        }
-
         // bind routes
-        $router->group([
+        $this->app->router->group([
             'middleware'    => [\Appstract\Opcache\Http\Middleware\Request::class],
             'prefix'        => 'opcache-api',
             'namespace'     => 'Appstract\Opcache\Http\Controllers',
         ], function ($router) {
             require __DIR__.'/Http/routes.php';
         });
-    }
-
-    private function isLumenWithoutRouterVersion()
-    {
-        $version = $this->app->version();
-        if (! str_contains($version, 'Lumen')) {
-            return false;
-        }
-
-        if (preg_match('/.*\((\d+\.\d+\.\d+)\).*/', $version, $m)) {
-            return version_compare('5.5', $m[1]) > 0;
-        }
-
-        return true;
     }
 }
