e512260bfd0eced3f533770b0a3b960f123d3172
Olav van Schie <olav@appstract.nl>
2019-12-31 21:06:57 +0100

    verify host fix
    # Conflicts:
    #	src/CreatesRequest.php

--- config/opcache.php
+++ config/opcache.php
@@ -3,7 +3,7 @@
 return [
     'url' => env('OPCACHE_URL', config('app.url')),
     'verify_ssl' => true,
-    'verify_host' => true,
+    'verify_host' => 2, // 0 for disabled
     'headers' => [],
     'directories' => [
         base_path('app'),
